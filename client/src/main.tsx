23b4443704fcd889876ed630269fd2d6f0cb2ea5
Ismini Papa <ismini.papa@hotmai.gr>
2024-06-15 11:40:14 +0100

    Tic tac toe - complete first stage

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,22 +1,42 @@
-import React, { useState } from 'react'
-import { XorO } from './types'
-
+import React, { useState } from "react";
+import { useGameStore } from "./store";
 
 export const Main = () => {
-  const [board, setBoard] = useState<(XorO | undefined)[][]>([
-    [undefined, undefined, undefined],
-    [undefined, undefined, undefined],
-    [undefined, undefined, undefined]
-  ])
+  const { board, currentPlayer, winner, handleClick, resetGame } =
+    useGameStore();
 
-  return <div className='flex flex-col mt-10 items-center gap-10'>
-    <div className='font-bold text-2xl'>Tic Tac Toe</div>
-    <div className='flex flex-col gap-1'>
-      {board.map(row => <div className='flex gap-1'>
-        {row.map(column => <div className='border-2 border-gray-900 w-10 h-10 cursor-pointer items-center justify-center text-2xl font-bold flex'>
-          {column}
-        </div>)}
-      </div>)}
+  console.log("currentPlayer", currentPlayer);
+
+  return (
+    <div className="flex flex-col mt-10 items-center gap-10">
+      <div className="font-bold text-2xl">Tic Tac Toe</div>
+      <div className="font-bold text-xl">Time for {currentPlayer} to play</div>
+      <div className="flex flex-col gap-1">
+        {board.map((row, rowIndex) => (
+          <div className="flex gap-1" key={rowIndex}>
+            {row.map((cell, colIndex) => (
+              <div
+                key={colIndex}
+                className="border-2 border-gray-900 w-10 h-10 cursor-pointer items-center justify-center text-2xl font-bold flex"
+                onClick={() => handleClick(rowIndex, colIndex)}
+              >
+                {cell}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+      {winner && (
+        <div className="mt-4 font-bold text-xl">
+          {winner === "Draw" ? "No winner" : `Winner: ${winner}`}
+        </div>
+      )}
+      <button
+        className="mt-4 py-2 px-4 border-2 border-gray-900 text-xl"
+        onClick={resetGame}
+      >
+        Reset Game
+      </button>
     </div>
-  </div>
-}
+  );
+};
